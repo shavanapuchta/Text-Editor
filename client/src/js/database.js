088ba8a903a07ec813a7659bdd30869e5282a018
shavanapuchta <119765380+shavanapuchta@users.noreply.github.com>
2023-06-07 21:15:36 -0700

    added TODOs for database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,36 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to add a method that accepts some content and then adds it to the database
+export const putDb = async (content) => {
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.add(content);
+    await tx.complete;
+    console.log('Conent added to database', content);
+  } catch (err) {
+    console.error('Error: Could not add content to database', error);
+  }
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Logic to add a method that gets all the content from the database
+export const getDb = async () => {
+    try {
+        const db = await initdb();
+        const tx = db.transaction('jate', 'readonly');
+        const store = tx.objectStore('jate');
+        const allContent = await store.getAll();
+        await tx.complete;
+        console.log('Content retreived from database:', allContent);
+        return allContent;
+    } catch (err) {
+        console.error('Error: Could not retrieve content from database:', err);
+        return[];
+    }
+};
+
+console.error('getDb not implemented');
 
 initdb();
